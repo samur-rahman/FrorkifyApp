e7b6c4e3c766a36b48ebebeaf15523a2505110f4
samur-rahman <ahotasam@buffalo.edu>
2022-09-05 22:42:49 -0400

    Added hashchange and load event

--- src/js/controller.js
+++ src/js/controller.js
@@ -32,11 +32,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
     // loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -167,3 +171,5 @@ const showRecipe = async function () {
 };
 
 showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
