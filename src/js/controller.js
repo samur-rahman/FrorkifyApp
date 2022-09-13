4cd7d6d046eee5b9cec4f481893875e85eb7df30
samur-rahman <ahotasam@buffalo.edu>
2022-09-13 18:34:19 -0400

    moved hashchange, click event to recipeview and implemented publisher-subscriber pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -18,10 +18,11 @@ const controlRecipes = async function () {
     // Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
